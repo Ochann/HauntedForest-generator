cffc94ff17b8e4151b98818a47b39305bac66eb1
Ochann <804580673@qq.com>
2024-04-06 00:18:04 +0100

    added update tilemap

--- Assets/Generator/Generator.cs
+++ Assets/Generator/Generator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using System;
+using UnityEngine.XR;
+using UnityEngine.Tilemaps;
 
 public class Generator : MonoBehaviour
 {
@@ -12,52 +14,90 @@ public class Generator : MonoBehaviour
 
     int[,] grid;
 
-    private void Start()
-    {
+    public Tilemap tilemap;
+    public TileBase terrain_0;
+
+    private void Start() {
         GenerateGrid();
     }
 
-    private void GenerateGrid()
-    {
+    private void GenerateGrid() {
         grid = new int[width, height];
         System.Random rand = new System.Random();
-        for (int x = 0; x < width; x++) 
-        {
-            for (int y = 0; y < height; y++)
-            {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 grid[x,y] = (rand.Next(0,100) < fillRate) ? 1: 0;
             }
         }
     }
 
+    private void CellularAutomata() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int walls = GetSurroundingWalls(x, y);
+                if (walls > 4)
+                    grid[x,y] = 1;
+                else if(walls < 4)
+                    grid[x,y] = 0;
+            }
+        }
+    }
 
-    //int size = 5;
-    //void Update()
-    //{
-    //    if (Input.GetMouseButtonDown(0))
-    //    {
-    //        Debug.Log("-1");
-    //        size --;
-    //    }
-    //}
+    private int GetSurroundingWalls(int gridX, int gridY) {
+        int surroundingWalls = 0;
+        for(int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++) {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++) {
+                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height) {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        surroundingWalls += grid[neighbourX, neighbourY];
+                    }
+                }
+                else {
+                    surroundingWalls++;
+                }
+            }
+        }
 
-    //void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawCube(new Vector3(0, 0, 0), new Vector3(size,size,size));
-    //}
+        return surroundingWalls;
+    }
 
-    void OnDrawGizmos()
-    {
-        if (grid != null)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("CA");
+            CellularAutomata();
+            UpDateTilemap();
+        }
+        else if (Input.GetMouseButtonDown(1)) {
+            Debug.Log("init");
+            GenerateGrid();
+        }
+    }
+
+
+    private void OnDrawGizmos() {
+        if (grid != null) {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
                     Gizmos.color = (grid[x, y] == 1) ? Color.black : Color.white;
-                    Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+                    Vector3 pos = new Vector3(-width / 2 + x + .5f, -height / 2 + y + .5f, 0);
                     Gizmos.DrawCube(pos, Vector3.one);
+                }
+            }
+        }
+    }
+
+    private void UpDateTilemap() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Vector3Int pos = new Vector3Int(x, y, 0) - new Vector3Int(width/2, height/2, 0);
+                if (grid[x, y] == 1) {
+                    
+                    tilemap.SetTile(pos, terrain_0);
+                }
+                else
+                {
+                    tilemap.SetTile(pos, null);
                 }
             }
         }
